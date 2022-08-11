b71398c894abdf32a284b02b8c6d07c06cf120e9
Phạm Tuấn Nghĩa <52267082+phamtuannghia22@users.noreply.github.com>
2022-08-11 16:09:03 +0700

    thêm file css cho global

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/css/main.css'
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 /* eslint-disable no-new */
