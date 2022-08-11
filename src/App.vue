372b8ae875dbd05b47b94468814431a128f7c865
Phạm Tuấn Nghĩa <52267082+phamtuannghia22@users.noreply.github.com>
2022-08-11 16:12:31 +0700

    fix UI

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,6 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Directive.vue
+++ src/components/Directive.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="directive">
-    <h1 class="text-primary my-3 text-center">Directive</h1>
+    <h1 class="text-primary mt-3 mb-0 text-center">Directive</h1>
     <!--v-on-->
     <div id="v-on" class="my-5 p-5 container-center mx-auto">
       <h2 class="fw-bold text-danger">V-on</h2>
@@ -21,7 +21,7 @@
     <!--end v-on-->
     <!--V-bind-->
     <div class="my-5 p-5 container-center mx-auto">
-      <h1 class="text-danger my-3 text-center">V-bind</h1>
+      <h2 class="text-danger fw-bold my-3 text-center">V-bind</h2>
       <a class="p-5" v-bind:href="url">Wuyi Art</a>
     </div>
     <!--end v-bind-->
