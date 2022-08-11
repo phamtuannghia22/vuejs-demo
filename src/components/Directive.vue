74637e168395f5eb17942128734f747d3934230d
Phạm Tuấn Nghĩa <52267082+phamtuannghia22@users.noreply.github.com>
2022-08-11 16:09:20 +0700

    coding directive

--- src/components/Directive.vue
+++ src/components/Directive.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
+  <div id="directive">
+    <h1 class="text-primary my-3 text-center">Directive</h1>
     <!--v-on-->
-    <div>
-      <h2>V-on</h2>
+    <div id="v-on" class="my-5 p-5 container-center mx-auto">
+      <h2 class="fw-bold text-danger">V-on</h2>
+      <!--Click-->
       <p>Count: {{ count }}</p>
-      <button v-on:click="increaseCount()">Click</button>
+      <div class="d-flex w-100 justify-content-between">
+        <button class="me-3" v-on:click="increaseCount()">Increase</button>
+        <button v-on:click="reduceCount()">Reduce</button>
+      </div>
+      <!--Hover-->
+      <div class="d-flex mt-5 justify-content-between">
+        <div id="hover-me" v-on:mousemove="hover($event)">
+          <span class="m-auto text-white"> Hover me </span>
+          <p class="mt-4 text-white">{{ position }}</p>
+        </div>
+      </div>
     </div>
+    <!--end v-on-->
+    <!--V-bind-->
+    <div class="my-5 p-5 container-center mx-auto">
+      <h1 class="text-danger my-3 text-center">V-bind</h1>
+      <a class="p-5" v-bind:href="url">Wuyi Art</a>
+    </div>
+    <!--end v-bind-->
   </div>
 </template>
 
@@ -14,12 +33,21 @@ export default {
   name: 'Directive',
   data () {
     return {
-      count: 0
+      count: 0,
+      position: 'x = 0  y = 0',
+      url: 'https://phamtuannghia22.github.io/wuyi-art/'
     }
   },
   methods: {
     increaseCount () {
       this.count++
+    },
+    reduceCount () {
+      this.count--
+    },
+    hover (e) {
+      console.log(e)
+      this.position = `x = ${e.offsetX}  y = ${e.offsetY}`
     }
   }
 }
