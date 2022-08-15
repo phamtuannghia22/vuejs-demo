1941a6bcadaa016a05fb0f4949962557f7abd3be
Phạm Tuấn Nghĩa <52267082+phamtuannghia22@users.noreply.github.com>
2022-08-15 14:04:22 +0700

    update vue bind

--- src/components/Directive.vue
+++ src/components/Directive.vue
@@ -1,40 +1,49 @@
 <template>
   <div id="directive">
     <h1 class="text-primary mt-3 mb-0 text-center">Directive</h1>
+    <div class="row container">
 
-    <!--v-on-->
-    <div id="v-on" class="my-5 p-5 container-center mx-auto">
-      <h2 class="fw-bold text-danger">V-on</h2>
-      <!--Click-->
-      <p>Count: {{ count }}</p>
-      <div class="d-flex w-100 justify-content-between">
-        <button class="me-3" v-on:click="increaseCount()">Increase</button>
-        <button v-on:click="reduceCount()">Reduce</button>
-      </div>
-      <!--Hover-->
-      <div class="d-flex mt-5 justify-content-between">
-        <div id="hover-me" v-on:mousemove="hover($event)">
-          <span class="m-auto text-white"> Hover me </span>
-          <p class="mt-4 text-white">{{ position }}</p>
+      <!--v-on-->
+      <div id="v-on" class="m-auto p-5 container-center mx-auto">
+        <h2 class="fw-bold text-danger">V-on</h2>
+        <!--Click-->
+        <p>Count: {{ count }}</p>
+        <div class="d-flex w-100 justify-content-between">
+          <button class="me-3" v-on:click="increaseCount()">Increase</button>
+          <button v-on:click="reduceCount()">Reduce</button>
+        </div>
+        <!--Hover-->
+        <div class="d-flex mt-5 justify-content-between">
+          <div id="hover-me" v-on:mousemove="hover($event)">
+            <span class="m-auto text-white"> Hover me </span>
+            <p class="mt-4 text-white">{{ position }}</p>
+          </div>
         </div>
       </div>
-    </div>
-    <!--end v-on-->
+      <!--end v-on-->
 
-    <!--V-bind-->
-    <div class="my-5 p-5 container-center mx-auto">
-      <h2 class="text-danger fw-bold my-3 text-center">V-bind</h2>
-      <a class="p-5" v-bind:href="url">Wuyi Art</a>
-    </div>
-    <!--end v-bind-->
+      <!--V-bind-->
+      <div id="v-bind" class="m-auto p-5 container-center mx-auto">
+        <h2 class="text-danger fw-bold my-3 text-center">V-bind</h2>
+        <a class="p-5" v-bind:href="url">Wuyi Art</a>
+        <h4 class="text-danger fw-bold my-3 text-center">Ràng buộc class</h4>
+        <div class="d-flex justify-content-between">
+          <div id="vbind-div" v-bind:class="{'bg-red': isBackgroundRed}"></div>
+          <button class="ms-3 my-auto" v-on:click="changeBackground()">Change background</button>
+        </div>
+      </div>
+      <!--end v-bind-->
 
-    <!-- V model -->
-    <div class="my-5 p-5 container-center mx-auto">
-      <h2 class="text-danger fw-bold text-center">V-model (Two-way binding)</h2>
-      <p>{{userName}}</p>
-      <input v-model="userName" type="text" placeholder="user name" />
+      <!-- V model -->
+      <div class="m-auto p-5 container-center mx-auto">
+        <h2 class="text-danger fw-bold text-center">V-model (Two-way binding)</h2>
+        <p>{{userName}}</p>
+        <input v-model="userName" type="text" placeholder="user name" />
+      </div>
+
+      <!-- End v-model -->
     </div>
-    <!-- End v-model -->
+
   </div>
 </template>
 
@@ -43,10 +52,13 @@ export default {
   name: 'Directive',
   data () {
     return {
-      count: 0,
-      position: 'x = 0  y = 0',
-      url: 'https://phamtuannghia22.github.io/wuyi-art/',
-      userName: ''
+      count: 0, // dùng trong v-on
+      position: 'x = 0  y = 0', // dùng trong v-on
+
+      url: 'https://phamtuannghia22.github.io/wuyi-art/', // dùng trong v-bind
+      isBackgroundRed: true, // dùng trong v-bind
+
+      userName: '' // dùng trong v-model
     }
   },
   methods: {
@@ -59,7 +71,11 @@ export default {
     hover (e) {
       console.log(e)
       this.position = `x = ${e.offsetX}  y = ${e.offsetY}`
+    },
+    changeBackground () {
+      this.isBackgroundRed = !this.isBackgroundRed
     }
+
   }
 }
 </script>
