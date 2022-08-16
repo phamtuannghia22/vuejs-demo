5bac643136de624ca0791467d552569262e42857
Pham Tuan Nghia <nghia.pt228@gmail.com>
2022-08-16 17:47:59 +0700

    coding ref

---
+++ src/pages/Ref.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="text-primary mt-3 mb-0 text-center">Ref</h1>
+    <div class="my-5 p-5 container-center mx-auto">
+      <input v-on:change="change()" class="d-none" ref="RefInput" type="file" />
+      <button v-on:click="chooseFile()">Choose file</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ref',
+  methods: {
+    chooseFile () {
+      this.$refs.RefInput.click()
+      console.log(this.$refs.RefInput.files)
+    },
+    change () {
+      console.log(this.$refs.RefInput.files)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
