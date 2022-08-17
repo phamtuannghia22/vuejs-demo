370ed75daa1d1afc618f9e258b3a857aafc9791b
Pham Tuan Nghia <nghia.pt228@gmail.com>
2022-08-17 10:16:55 +0700

    update UI for data binding

--- src/pages/DataBinding.vue
+++ src/pages/DataBinding.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <h1>{{ titleDataBinding }}</h1>
-    <h4>{{ body.number }}</h4>
+    <h1 class="text-primary mt-3 mb-0 text-center">Data binding</h1>
+    <div class="my-5 p-5 container-center mx-auto">
+      <h1>{{ titleDataBinding }}</h1>
+      <h4>{{ body.number }}</h4>
+    </div>
   </div>
 </template>
 
